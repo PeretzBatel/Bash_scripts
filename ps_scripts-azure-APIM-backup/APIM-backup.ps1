9e41e79d60cde50a1b1ba44fd0f8a3a35a49cbe8
PeretzBatel <peretzbatel123@gmail.com>
2022-11-16 10:40:25 +0200

    Adding storage account

--- ps_scripts-azure-APIM-backup/APIM-backup.ps1
+++ ps_scripts-azure-APIM-backup/APIM-backup.ps1
@@ -2,3 +2,5 @@ Connect-AzureRmAccount
 
 #Create new azure storage account
 New-AzureRmStorageAccount -StorageAccountName "BatelStorage" -Location 'East US' -ResourceGroupName "DevGroup" -Type Standart_LRS
+#Storage Key
+$storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName "DevGroup" -StorageAccountName "BatelStorage")[0].Value
