c5f8f5dd97e9762e2f84d2938cda5e79005481da
PeretzBatel <peretzbatel123@gmail.com>
2022-11-16 10:46:07 +0200

    Creating container to host Backup

--- ps_scripts-azure-APIM-backup/APIM-backup.ps1
+++ ps_scripts-azure-APIM-backup/APIM-backup.ps1
@@ -4,3 +4,7 @@ Connect-AzureRmAccount
 New-AzureRmStorageAccount -StorageAccountName "BatelStorage" -Location 'East US' -ResourceGroupName "DevGroup" -Type Standart_LRS
 #Storage Key
 $storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName "DevGroup" -StorageAccountName "BatelStorage")[0].Value
+#Storge Context
+$storgeContext = NewAzureStorageContext -StorageAccountName "BatelStorage" -AzureRmStorageAccountKey $storageKey
+#Creating New container in the existing storge account - The container will host APIM backup-blob
+New-AzureStorageContainer -Name "apimbackupscontainer" -Permissiom Off -Context $storgeContext
